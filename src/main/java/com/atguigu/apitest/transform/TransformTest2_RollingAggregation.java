4b76619afb36c0826c88d98977acd05da3cd46a3
wushengran <shengran.wu@163.com>
2020-11-09 17:06:16 +0800

    Day 3: window api

--- src/main/java/com/atguigu/apitest/transform/TransformTest2_RollingAggregation.java
+++ src/main/java/com/atguigu/apitest/transform/TransformTest2_RollingAggregation.java
@@ -10,11 +10,15 @@ package com.atguigu.apitest.transform;/**
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import sun.awt.SunHints;
+
+import javax.xml.crypto.Data;
 
 /**
  * @ClassName: TransformTest2_RollingAggregation
@@ -25,7 +29,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class TransformTest2_RollingAggregation {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+        env.setParallelism(4);
 
         // 从文件读取数据
         DataStream<String> inputStream = env.readTextFile("D:\\Projects\\BigData\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
@@ -47,13 +51,24 @@ public class TransformTest2_RollingAggregation {
         // 分组
         KeyedStream<SensorReading, Tuple> keyedStream = dataStream.keyBy("id");
         KeyedStream<SensorReading, String> keyedStream1 = dataStream.keyBy(data -> data.getId());
-//        KeyedStream<SensorReading, String> keyedStream1 = dataStream.keyBy(SensorReading::getId);
+
+        DataStream<Long> dataStream1 = env.fromElements(1L, 34L, 4L, 657L, 23L);
+        KeyedStream<Long, Integer> keyedStream2 = dataStream1.keyBy(new KeySelector<Long, Integer>() {
+            @Override
+            public Integer getKey(Long value) throws Exception {
+                return value.intValue() % 2;
+            }
+        });
+
+        //        KeyedStream<SensorReading, String> keyedStream1 = dataStream.keyBy(SensorReading::getId);
 
         // 滚动聚合，取当前最大的温度值
         DataStream<SensorReading> resultStream = keyedStream.maxBy("temperature");
 
-        resultStream.print();
+        resultStream.print("result");
 
+        keyedStream1.print("key1");
+        keyedStream2.sum(0).print("key2");
         env.execute();
     }
 }
